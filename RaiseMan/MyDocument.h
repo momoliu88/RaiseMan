e369dec08d982282549866552f5dbd3d5e88c2cb
liuxiaoqin <momoliu88@126.com>
2013-06-09 15:01:19 +0800

    Initial Commit

---
+++ RaiseMan/MyDocument.h
@@ -0,0 +1,13 @@
+//
+//  MyDocument.h
+//  RaiseMan
+//
+//  Created by xiaoqinliu on 13-6-9.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+
+@end
---
+++ RaiseMan/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RaiseMan
+//
+//  Created by xiaoqinliu on 13-6-9.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
